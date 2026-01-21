c2e0e23a846306e80bb2148af7cab57fd91b6bc6
Phúc Lợi <huynhphucloi007@gmail.com>
2026-01-22 00:20:37 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+const textarea = document.getElementById("textarea");
+const tagsEl = document.getElementById("tags");
+textarea.focus();
+textarea.addEventListener("keyup", (e) => {
+    createTags(e.target.value);
+    if (e.key === "Enter") {
+        setTimeout(() => {
+            e.target.value = "";
+        }, 10);
+
+        randomSelect();
+    }
+});
+
+function createTags(input) {
+    const tags = input
+        .split(",")
+        .map(tag => tag.trim())
+        .filter(tag => tag !== "");
+    tagsEl.innerHTML = "";
+    tags.forEach(tag => {
+        const span = document.createElement("span");
+        span.classList.add("tag");
+        span.innerText = tag;
+        tagsEl.appendChild(span);
+    });
+}
+
+function randomSelect() {
+    const times = 30;
+
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag();
+
+        if (randomTag) {
+            highlightTag(randomTag);
+
+            setTimeout(() => {
+                unHighlightTag(randomTag);
+            }, 100);
+        }
+    }, 100);
+
+    setTimeout(() => {
+        clearInterval(interval);
+
+        setTimeout(() => {
+            const randomTag = pickRandomTag();
+            highlightTag(randomTag);
+        }, 100);
+    }, times * 100);
+}
+
+function pickRandomTag() {
+    const tags = document.querySelectorAll(".tag");
+    return tags[Math.floor(Math.random() * tags.length)];
+}
+
+function highlightTag(tag) {
+    tag.classList.add("highlight");
+}
+
+function unHighlightTag(tag) {
+  tag.classList.remove("highlight");
+}
